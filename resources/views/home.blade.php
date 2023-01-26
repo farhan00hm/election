db05c27e0c450d155c1d555aa0bdcc8d9c7313f1
Farhan <farhan96.hm@gmail.com>
2023-01-27 01:41:17 +0600

    working on auto search

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,14 @@
 @extends('main-template')
 @section('body')
     <div id="search-container">
-        <input id="search-input" type="text" placeholder="প্রার্থী বা আসনের নাম লিখুন" name="search">
-        <button type="submit" id ="search-button"><i class="fa fa-search"></i></button>
+        <input id="search-input" type="text" placeholder="প্রার্থীর নাম লিখুন" name="search">
+        <div>
+            <ul class="search-menu" role="menu" aria-labelledby="dLabel" id="search-menu">
+                <li>No Data sfasdf sdf sdfsad fasdf</li>
+                <li>No Data sfasfdsf df sdf sdfsad fasdf</li>
+                <li>No Data fsd dssfasdf sdf sdfsad fasdf</li>
+            </ul>
+        </div>
+{{--        <button type="submit" id ="search-button"><i class="fa fa-search"></i></button>--}}
     </div>
 @endsection
